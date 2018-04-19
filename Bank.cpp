566c1e2ca912fda4144e577d07f2392b20e4ade5
Ally Oliphant <aoliphant27@gmail.com>
2018-04-18 19:58:25 -0600

    correct bank files

--- Bank.cpp
+++ Bank.cpp
@@ -1,12 +1,50 @@
-#include "stdafx.h"
-#include "Bank.h"
+#pragma once
+#include "Account.h"
+#include "Customer.h"
+#include "SavingsAccount.h"
+#include <string>
+#include <iostream>
+#include <unordered_set>
 
+using namespace std;
 
-Bank::Bank()
+class Bank
 {
-}
-
-
-Bank::~Bank()
-{
-}
+public:
+	std::unordered_multiset<Account> accounts;
+	
+	void addAccount(Account account)
+	{
+		accounts.insert(account);
+	}
+	
+	void accrue(double rate)
+	{
+		for (auto a = accounts.begin(); a != accounts.end(); ++a) 
+		{
+			a.accrue(rate);
+		}
+	}
+	
+	string toString() 
+	{
+		string r = "";
+		for (auto a = accounts.begin(); a != accounts.end(); ++a) 
+		{
+			r.append(a.toString());
+			r.append("\n");
+		}
+		return r;
+	}
+	
+	int main()
+	{
+		Bank bank;
+		Customer c = new Customer("Ann");
+		bank.addAccount(new SavingsAccount("01002",c,200.00));
+		bank.accrue(0.02);
+		cout << bank.toString();
+		return 0;
+	}
+	
+};
--- Bank.h
+++ Bank.h
@@ -1,50 +1,12 @@
-#pragma once
-#include "Account.h"
-#include "Customer.h"
-#include "SavingsAccount.h"
-#include <string>
-#include <iostream>
-#include <unordered_set>
+#include "stdafx.h"
+#include "Bank.h"
 
-using namespace std;
-
-class Bank
+Bank::Bank()
 {
-public:
-	std::unordered_multiset<Account> accounts;
-	
-	void addAccount(Account account)
-	{
-		accounts.insert(account);
-	}
-	
-	void accrue(double rate)
-	{
-		for (auto a = accounts.begin(); a != accounts.end(); ++a) 
-		{
-			a.accrue(rate);
-		}
-	}
-	
-	string toString() 
-	{
-		string r = "";
-		for (auto a = accounts.begin(); a != accounts.end(); ++a) 
-		{
-			r.append(a.toString());
-			r.append("\n");
-		}
-		return r;
-	}
-	
-	int main()
-	{
-		Bank bank;
-		Customer c = new Customer("Ann");
-		bank.addAccount(new SavingsAccount("01002",c,200.00));
-		bank.accrue(0.02);
-		cout << bank.toString();
-		return 0;
-	}
-	
-};
+}
+
+
+Bank::~Bank()
+{
+}
+
